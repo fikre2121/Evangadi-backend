0893462e9e7d8b50df3c13faea9d1edec7ade6c6
Fikre2121 <fikre.gebremariam2121@gmail.com>
2025-08-30 11:35:23 +0300

    user registratoin

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,52 @@
-function register(req,res){
- res.end("register")
+const dbconnection = require("../db/dbconfigl");
+const bcrypt = require("bcrypt");
+const { StatusCodes } = require("http-status-codes");
+
+
+async function register(req, res) {
+  const { username, firstname, lastname, email, password } = req.body;
+
+  if (!email || !lastname || !firstname || !password || !username) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "please provide all required information" });
+  }
+  try {
+    const [existing] = await dbconnection.query(
+      `SELECT username,userid FROM users where email =? or username = ?`,
+      [email, username]
+    );
+    // check if the user is existed
+    if (existing.length > 0) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ msg: "usere already existed" });
+    }
+    // check the password streangth
+    if (password.length <= 8) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "password should be atleast 8 characters" });
+    }
+
+    // incrpt the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+    await dbconnection.query(
+      "INSERT INTO users(username, firstname, lastname, email, password) VALUES(?,?,?,?,?)",
+      [username, firstname, lastname, email, hashedPassword]
+    );
+
+    return res.status(StatusCodes.CREATED).json({ msg: "User registered successfully" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "somthing went wrong,try again later" });
+  }
 }
 
-function login(req, res) {
+async function login(req, res) {
   res.end("login");
 }
-function check(req, res) {
+async function check(req, res) {
   res.end("check");
 }
- 
-module.exports={register,login,check}
+
+module.exports = { register, login, check };
--- main.js
+++ main.js
@@ -1,48 +1,38 @@
 const express = require("express");
-const app = express(); 
-const port=4000
+const app = express();
+const port = 4000;
 // db connection
 
-const dbconnection= require("./db/dbconfigl")
+const dbconnection = require("./db/dbconfigl");
 // user routs middleware file
-const userRoutes=require("./routs/userRouter")
+const userRoutes = require("./routs/userRouter");
 
-// user routs middleware 
-app.use("/api/users",userRoutes)
+// json middleware to extract json data
+
+app.use(express.json());
+
+// user routs middleware
+app.use("/api/users", userRoutes);
 
 // qostion routes middleware??
 
 // answer routes middleware??
 
+async function start() {
+  try {
+    const rows = await dbconnection.execute("select 'test' ");
+    app.listen(port);
 
+    console.log(rows);
 
- async function start(){
-try {
-  const [rows] = await dbconnection.execute("select 'test' ");
-console.log(rows)
+    console.log("database connection established");
 
-} catch (error) {
-  console.log(error.message);
-}
-
-}
-start()
-
-app.listen(port, (err) => {
-  if(err){
-console.log(err.message)
-  }else{
-
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`listening on ${port}`);
+  } catch (error) {
+    console.log(error.message);
   }
-  
-});
-
-
-
-
-
-
+}
+start();
 
 // app.get("/", (req, res) => {
 //   res.json({ name: "fikre kindeya", status: "success" });
