3201a2b04a2c513cf7012e620d9ec8477b7f9405
Fikre2121 <fikre.gebremariam2121@gmail.com>
2026-07-02 10:21:49 +0300

    the server is updated

--- main.js
+++ main.js
@@ -1,45 +1,48 @@
 const express = require("express");
-const app = express();
- const cors=require("cors")
- app.use(cors())
-const port = 4000;
-// db connection
+const cors = require("cors");
 
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+// Database connection
 const dbconnection = require("./db/dbconfigl");
-//  routs middleware fils
+
+// Routes
 const userRoutes = require("./routs/userRouter");
 const questionRoutes = require("./routs/questionRoutes");
 const answerRoutes = require("./routs/answerRouts");
-// json middleware to extract json data
 
+// Middlewares
 app.use(express.json());
 
-// user routs middleware
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://golden-medovik-db5600.netlify.app",
+    ],
+    credentials: true,
+  }),
+);
+
+// Routes
 app.use("/api/users", userRoutes);
-
-// qostion routes middleware??
-
-app.use("/api/questions",questionRoutes);
-
-// answer routes middleware??
-
-app.use("/api/answers",answerRoutes);
-
+app.use("/api/questions", questionRoutes);
+app.use("/api/answers", answerRoutes);
 
 async function start() {
   try {
-    const rows = await dbconnection.execute("select 'test' ");
-    app.listen(port);
+    await dbconnection.execute("SELECT 'test'");
 
-    console.log(rows);
+    console.log("Database connection established.");
 
-    console.log("database connection established");
-
-    console.log(`listening on ${port}`);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
   } catch (error) {
-    console.log(error.message);
+    console.error(error.message);
   }
 }
+
 start();
-
-
